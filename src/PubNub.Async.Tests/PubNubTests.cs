2b15d4d17af78d4380cd70a1ef09c3f64809895b
Brian Engen <engenb@gmail.com>
2016-05-01 16:58:10 -0500

    initial pubnub client ctors

---
+++ src/PubNub.Async.Tests/PubNubTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace PubNub.Async.Tests
+{
+	public class PubNubTests : AbstractTest
+	{
+		[Fact]
+		public void ctor__Given_PubAndSubKeys__Then_EmptyNonProvidedSSLTrue()
+		{
+			var expectedPubKey = Fixture.Create<string>();
+			var expectedSubKey = Fixture.Create<string>();
+			var expectedSecKey = string.Empty;
+			var expectedCiphKey = string.Empty;
+			var expectedSsl = true;
+
+			var subject = new PubNub(expectedPubKey, expectedSubKey);
+
+			Assert.Equal(expectedPubKey, subject.PublishKey);
+			Assert.Equal(expectedSubKey, subject.SubscribeKey);
+			Assert.Equal(expectedSecKey, subject.SecretKey);
+			Assert.Equal(expectedCiphKey, subject.CipherKey);
+			Assert.Equal(expectedSsl, subject.SslEnabled);
+
+			Assert.NotNull(subject.SessionUUID);
+			Assert.NotEmpty(subject.SessionUUID);
+			Guid outGuid;
+			Assert.True(Guid.TryParse(subject.SessionUUID, out outGuid));
+		}
+
+		[Fact]
+		public void ctor__Given_PubSubAndSecKeys__Then_EmptyNonProvidedSSLTrue()
+		{
+			var expectedPubKey = Fixture.Create<string>();
+			var expectedSubKey = Fixture.Create<string>();
+			var expectedSecKey = Fixture.Create<string>();
+			var expectedCiphKey = string.Empty;
+			var expectedSsl = true;
+
+			var subject = new PubNub(expectedPubKey, expectedSubKey, expectedSecKey);
+
+			Assert.Equal(expectedPubKey, subject.PublishKey);
+			Assert.Equal(expectedSubKey, subject.SubscribeKey);
+			Assert.Equal(expectedSecKey, subject.SecretKey);
+			Assert.Equal(expectedCiphKey, subject.CipherKey);
+			Assert.Equal(expectedSsl, subject.SslEnabled);
+
+			Assert.NotNull(subject.SessionUUID);
+			Assert.NotEmpty(subject.SessionUUID);
+			Guid outGuid;
+			Assert.True(Guid.TryParse(subject.SessionUUID, out outGuid));
+		}
+
+		[Fact]
+		public void ctor__Given_PubSubSecAndCiphKeys__Then_EmptyNonProvidedSSLTrue()
+		{
+			var expectedPubKey = Fixture.Create<string>();
+			var expectedSubKey = Fixture.Create<string>();
+			var expectedSecKey = Fixture.Create<string>();
+			var expectedCiphKey = Fixture.Create<string>();
+			var expectedSsl = true;
+
+			var subject = new PubNub(expectedPubKey, expectedSubKey, expectedSecKey, expectedCiphKey, expectedSsl);
+
+			Assert.Equal(expectedPubKey, subject.PublishKey);
+			Assert.Equal(expectedSubKey, subject.SubscribeKey);
+			Assert.Equal(expectedSecKey, subject.SecretKey);
+			Assert.Equal(expectedCiphKey, subject.CipherKey);
+			Assert.Equal(expectedSsl, subject.SslEnabled);
+
+			Assert.NotNull(subject.SessionUUID);
+			Assert.NotEmpty(subject.SessionUUID);
+			Guid outGuid;
+			Assert.True(Guid.TryParse(subject.SessionUUID, out outGuid));
+		}
+	}
+}
---
+++ src/PubNub.Async/PubNub.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PubNub.Async
+{
+	public class PubNub
+	{
+		public string SessionUUID { get; private set; }
+		
+		public string PublishKey { get; }
+		public string SubscribeKey { get; }
+		public string SecretKey { get; }
+		public string CipherKey { get; }
+		public bool SslEnabled { get; }
+
+		public PubNub(string publishKey, string subscribeKey) :
+			this(publishKey, subscribeKey, "")
+		{
+		}
+
+		public PubNub(string publishKey, string subscribeKey, string secretKey) :
+			this(publishKey, subscribeKey, secretKey, "", true)
+		{
+		}
+
+		public PubNub(string publishKey, string subscribeKey, string secretKey, string cipherKey, bool sslEnabled)
+		{
+			if (string.IsNullOrWhiteSpace(publishKey)) publishKey = string.Empty;
+			if (string.IsNullOrWhiteSpace(subscribeKey)) subscribeKey = string.Empty;
+			if (string.IsNullOrWhiteSpace(secretKey)) secretKey = string.Empty;
+			if (string.IsNullOrWhiteSpace(cipherKey)) cipherKey = string.Empty;
+
+			PublishKey = publishKey;
+			SubscribeKey = subscribeKey;
+			SecretKey = secretKey;
+			CipherKey = cipherKey;
+			SslEnabled = sslEnabled;
+
+			SessionUUID = Guid.NewGuid().ToString();
+		}
+	}
+}
