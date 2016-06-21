d3c953b5419e77938fb1fa2a60474af15d1de11e
Brian Engen <engenb@gmail.com>
2016-06-20 21:37:16 -0500

    adding required descriptions to AssemblyInfos for nuget

--- src/PubNub.Async.Push/Properties/AssemblyInfo.cs
+++ src/PubNub.Async.Push/Properties/AssemblyInfo.cs
@@ -1,18 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 
 [assembly: AssemblyTitle("PubNub.Async.Push")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Push notification extensions for PubNub.Async")]
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: AssemblyInformationalVersion("0.0.0.0")]
-
-[assembly: InternalsVisibleTo("PubNub.Async.Autofac"),
-           InternalsVisibleTo("PubNub.Async.Testing"),
-           InternalsVisibleTo("PubNub.Async.Tests")]
