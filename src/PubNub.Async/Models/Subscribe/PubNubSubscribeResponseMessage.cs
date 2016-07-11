ef500c718d84b5b014645b812e864f9d0b7e21b0
Brian Engen <engenb@gmail.com>
2016-07-10 22:19:15 -0500

    updated property names per pubnub correspondence

--- src/PubNub.Async/Models/Subscribe/PubNubSubscribeResponseMessage.cs
+++ src/PubNub.Async/Models/Subscribe/PubNubSubscribeResponseMessage.cs
@@ -10,20 +10,26 @@ namespace PubNub.Async.Models.Subscribe
 {
     public class PubNubSubscribeResponseMessage
     {
-        //"a": "3"
-        //"f": 0
+        [JsonProperty("a")]
+        public int Shard { get; set; }
+     
+        [JsonProperty("f")]
+        public int Flags { get; set; }
 
         [JsonProperty("i")]
         public string SessionUuid { get; set; }
         [JsonProperty("p")]
         public PubNubSubscribeResponseTime Processed { get; set; }
+
         [JsonProperty("k")]
         public string SubscribeKey { get; set; }
+
         [JsonProperty("c")]
         public string Channel { get; set; }
+        [JsonProperty("b")]
+        public string ChannelGroup { get; set; }
+
         [JsonProperty("d")]
         public JToken Data { get; set; }
-
-        //"b": "test2"
     }
 }
